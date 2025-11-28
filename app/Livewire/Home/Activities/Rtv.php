478987836fb29707b77965da8bfb345ff1eba640
John Michael Renon <jm.renon@DCMakati.local>
2025-11-28 16:34:01 +0800

    rtv reports

--- app/Livewire/Home/Activities/Rtv.php
+++ app/Livewire/Home/Activities/Rtv.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use Livewire\WithPagination;
 
 use App\Models\Product;
+use App\Models\DiserActivityRTV;
 use Illuminate\Support\Facades\Session;
 
 class Rtv extends Component
@@ -15,14 +16,22 @@ class Rtv extends Component
 
     public $search;
     public $rtv_data;
+    public $diser_login;
+    public $diser_activities;
 
     public function render()
     {
          $products = Product::orderBy('stock_code', 'ASC')
+            ->whereHas('price_codes', function($query) {
+                $query->where('code', $this->diser_login->account->price_code)
+                    ->where('company_id', $this->diser_login->account->company_id);
+            })
             ->when(!empty($this->search), function($query) {
-                $query->where('stock_code', 'LIKE', '%'.$this->search.'%')
-                    ->orWhere('description', 'LIKE', '%'.$this->search.'%')
-                    ->orWhere('size', 'LIKE', '%'.$this->search.'%');
+                $query->where(function($qry) {
+                    $qry->where('stock_code', 'LIKE', '%'.$this->search.'%')
+                        ->orWhere('description', 'LIKE', '%'.$this->search.'%')
+                        ->orWhere('size', 'LIKE', '%'.$this->search.'%');
+                });
             })
             ->paginate(6, ['*'], 'rtv-page');
 
@@ -32,7 +41,29 @@ class Rtv extends Component
     }
 
     public function mount() {
-        $this->freshness_data = [];
+        $this->rtv_data = [];
+        $this->diser_login = Session::get('diser_login');
+        $this->diser_activities = Session::get('diser_activities');
+
+        if(empty($this->diser_activities['rtv_data'])) {
+            $rtvs = DiserActivityRTV::where('diser_activity_id', $this->diser_activities['activity']->id)
+                ->get();
+            foreach($rtvs as $rtv) {
+                $this->rtv_data[$rtv->product_id] = [
+                    'rtv_number' => $rtv->rtv_number ?? '',
+                    'reason' => $rtv->reason ?? '',
+                    'inventory_count' => $rtv->inventory_count ?? ''
+                ];
+            }
+        } else {
+            foreach($this->diser_activities['rtv_data'] as $product_id => $data) {
+                $this->rtv_data[$product_id] = [
+                    'rtv_number' => $data['rtv_number'] ?? '',
+                    'reason' => $data['reason'] ?? '',
+                    'inventory_count' => $data['inventory_count'] ?? ''
+                ];
+            }
+        }
     }
 
     public function updatedSearch() {
@@ -40,13 +71,38 @@ class Rtv extends Component
     }
 
     public function saveSession() {
-        $diser_activities = Session::get('diser_activities');
-        $diser_activities['rtv_data'] = $this->rtv_data;
-
-        Session::put('diser_activities', $diser_activities);
+        $this->diser_activities['rtv_data'] = $this->rtv_data;
+        Session::put('diser_activities', $this->diser_activities);
     }
 
     public function updatedRtvData() {
+        $this->saveRTV();
         $this->saveSession();
+    }
+
+    public function saveRTV() {
+        foreach($this->rtv_data as $product_id => $data) {
+            if(!empty($data['rtv_number']) || !empty($data['reason']) || !empty($data['inventory_count'])) {
+                $rtv = DiserActivityRTV::where('diser_activity_id', $this->diser_activities['activity']->id)
+                    ->where('product_id', $product_id)
+                    ->first();
+                if(!empty($rtv)) {
+                    $rtv->update([
+                        'rtv_number' => !empty($data['rtv_number']) ? $data['rtv_number'] : NULL,
+                        'reason' => !empty($data['reason']) ? $data['reason'] : NULL,
+                        'inventory_count' => !empty($data['inventory_count']) ? $data['inventory_count'] : NULL,
+                    ]);
+                } else {
+                    $rtv = new DiserActivityRTV([
+                        'diser_activity_id' => $this->diser_activities['activity']->id,
+                        'product_id' => $product_id,
+                        'rtv_number' => !empty($data['rtv_number']) ? $data['rtv_number'] : NULL,
+                        'reason' => !empty($data['reason']) ? $data['reason'] : NULL,
+                        'inventory_count' => !empty($data['inventory_count']) ? $data['inventory_count'] : NULL,
+                    ]);
+                    $rtv->save();
+                }
+            }
+        }
     }
 }
