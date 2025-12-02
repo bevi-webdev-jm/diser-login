3db7d6894d5db45ce5ffcf3e5998cababce66afd
John Michael Renon <jm.renon@DCMakati.local>
2025-12-02 10:25:12 +0800

    total findings

--- resources/views/livewire/home/activities/total-findings.blade.php
+++ resources/views/livewire/home/activities/total-findings.blade.php
@@ -6,7 +6,15 @@
                 <input type="text" class="form-control form-control-sm" placeholder="Search" wire:model.live="search">
             </div>
         </div>
-        <div class="card-body p-0">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="form-group mb-0">
+                        <label for="total_findings">TOTAL FINDINGS</label>
+                        <textarea class="form-control form-control-sm" id="total_findings" wire:model.blur="total_findings"></textarea>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="card-footer">
         </div>
